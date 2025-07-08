f79feaa2744557d3d72be1a16447bd66fb43c7f8
vedansh pratap singh <vedanshpsingh@gmail.com>
2025-07-08 15:25:54 +0530

    fix

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 const plm = require('passport-local-mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/pin");
+require("dotenv").config(); // if not already in app.js
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("✅ MongoDB connected"))
+.catch(err => console.error("❌ DB error:", err));
 
 const userSchema = mongoose.Schema({
   username:{
