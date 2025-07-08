c338e34b3a28c1e43a684d7169859b7580f209e2
vedansh pratap singh <vedanshpsingh@gmail.com>
2025-07-08 16:35:25 +0530

    v2

--- app.js
+++ app.js
@@ -54,10 +54,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server is running on port ${PORT}`);
-});
 
 
 module.exports = app;
